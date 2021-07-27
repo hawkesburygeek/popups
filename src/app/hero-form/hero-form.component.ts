5ade4455e874d6896b1f462d7d5db5273df986d5
hawkgeek <therapturevoid@gmail.com>
2021-07-27 16:56:58 -0400

    create page

---
+++ src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.css']
+})
+export class HeroFormComponent {
+
+  shoots = ['Photoshoot', 'Video Shoot'];
+  schedulings = ['Auto Fill Default for requested', 'Auto Fill Default for requested'];
+
+  model = new Hero(0, '', '', this.shoots[0], 'Auto Fill Default for requested');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+
+  newHero() {
+    this.model = new Hero(42, '', '', '');
+  }
+
+  skyDog(): Hero {
+    const myHero =  new Hero(42, 'SkyDog',
+                           'Fetch any object at any distance',
+                           'Leslie Rollover');
+    console.log('My hero is called ' + myHero.name); // "My hero is called SkyDog"
+    return myHero;
+  }
+
+  //////// NOT SHOWN IN DOCS ////////
+
+  // Reveal in html:
+  //   Name via form.controls = {{showFormControls(heroForm)}}
+  showFormControls(form: any) {
+    return form && form.controls.name &&
+    form.controls.name.value; // Dr. IQ
+  }
+
+  /////////////////////////////
+
+}
---
+++ src/app/hero.ts
@@ -0,0 +1,11 @@
+export class Hero {
+
+  constructor(
+    public id: number,
+    public name: string,
+    public descripiton: string,
+    public shoot: string,
+    public scheduling?: string
+  ) {  }
+
+}
